0ee5a3d035bc3dc492f16c400f1e9dfb8fcae5d7
Ross Palmer <rossbpalmer@gmail.com>
2024-08-24 21:28:37 -0600

    fix script

--- spark-submit.sh
+++ spark-submit.sh
@@ -5,8 +5,8 @@ DRIVER_PY=$1
 zip -r pdp.zip pdp
 
 pip install -t dependencies -r requirements.txt
-popd dependencies
-  zip -r ../dependencies.zip
-pushd
+pushd dependencies
+  zip -r ../dependencies.zip .
+popd
 
 spark-submit --py-files "pdp.zip,dependencies.zip,$DRIVER_PY" "$DRIVER_PY"
