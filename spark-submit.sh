51ab7d3fd0d764564c9e70e2577dff00c14b8bd8
Ross Palmer <rossbpalmer@gmail.com>
2024-08-24 21:11:20 -0600

    setup to package dependencies in spark submit

--- spark-submit.sh
+++ spark-submit.sh
@@ -4,4 +4,9 @@ DRIVER_PY=$1
 
 zip -r pdp.zip pdp
 
-spark-submit --py-files "pdp.zip,$DRIVER_PY" "$DRIVER"
+pip install -t dependencies -r requirements.txt
+popd dependencies
+  zip -r ../dependencies.zip
+pushd
+
+spark-submit --py-files "pdp.zip,dependencies.zip,$DRIVER_PY" "$DRIVER"
