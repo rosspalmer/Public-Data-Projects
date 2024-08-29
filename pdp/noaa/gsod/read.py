9b58131c451b00bf836af9f07753a947d565660f
Ross Palmer <rossbpalmer@gmail.com>
2024-08-29 14:40:39 -0600

    add basic silver daily job

--- pdp/noaa/gsod/read.py
+++ pdp/noaa/gsod/read.py
@@ -33,17 +33,17 @@ class GlobalSurfaceSummaryOfDay(SharedSpark):
             self.spark.sql(f"CREATE SCHEMA IF NOT EXISTS {self.schema(medallion)}")
 
         self._ingest_bronze_stations()
-        # self._ingest_bronze_daily()
         # self._generate_silver_stations()
+        self._ingest_bronze_daily()
+        self._generate_silver_daily()
 
     def _ingest_bronze_stations(self):
 
         df = self.spark.read.option("header", True) \
             .csv(f"{self.data_folder_path}/isd-history.csv") \
             .withColumnRenamed("STATION NAME", "STATION_NAME") \
-            .withColumnRenamed("ELEV(M)", "ELEV_M")
-
-        df.printSchema()
+            .withColumnRenamed("ELEV(M)", "ELEV_M") \
+            .withColumn("station_id", concat_ws("-", col("USAF"), col("WBAN")))
 
         df.write.mode("overwrite").format("delta") \
             .saveAsTable(self.STATIONS_BRONZE)
@@ -117,24 +117,24 @@ class GlobalSurfaceSummaryOfDay(SharedSpark):
             return string_col.cast(dtype).alias(name)
 
         df = df.select(
-            parse("stn", "int", 1, 6, False),
-            parse("wban", "int", 8, 5, False),
-            parse("year", "int", 15, 4),
-            parse("month", "int", 19, 2),
-            parse("day", "int", 21, 2),
-            parse("temp", "float", 25, 6),
+            parse("stn", "string", 1, 6, False),
+            parse("wban", "string", 8, 5, False),
+            parse("year", "string", 15, 4),
+            parse("month", "string", 19, 2),
+            parse("day", "string", 21, 2),
+            parse("temp", "decimal(5,1)", 25, 6),
             parse("temp_count", "int", 32, 2),
-            parse("dewp", "float", 36, 6),
+            parse("dewp", "decimal(5,1)", 36, 6),
             parse("dewp_count", "int", 43, 2),
-            parse("slp", "float", 47, 6),
+            parse("slp", "decimal(5,1)", 47, 6),
             parse("slp_count", "int", 54, 2),
-            parse("stp", "float", 58, 6),
+            parse("stp", "decimal(5,1)", 58, 6),
             parse("stp_count", "int", 65, 2),
-            parse("visib", "float", 69, 5),
+            parse("visib", "decimal(4,1)", 69, 5),
             parse("visib_count", "int", 75, 2),
-            parse("wdsp", "float", 79, 5),
+            parse("wdsp", "decimal(4,1)", 79, 5),
             parse("wdsp_count", "int", 85, 2),
-            parse("mxspd", "float", 89, 5),
+            parse("mxspd", "decimal(4,1)", 89, 5),
             parse("gust", "float", 96, 5),
             parse("max", "float", 103, 6),
             parse("max_flag", "string", 109, 1),
@@ -146,6 +146,8 @@ class GlobalSurfaceSummaryOfDay(SharedSpark):
             parse("frshtt", "string", 133, 6)
         )
 
+        df = df.select(concat_ws("-", col("USAF"), col("WBAN")).alias("station_id"))
+
         df.write.mode("overwrite").format("delta") \
             .option("optimizeWrite", "True") \
             .saveAsTable(self.STATIONS_SILVER)
@@ -154,6 +156,24 @@ class GlobalSurfaceSummaryOfDay(SharedSpark):
 
         df = self.spark.table(self.DAILY_BRONZE) \
             .select(
-                concat_ws("-", col("USAF"), col("WBAN")).alias("station_id")
+                col("station_id"),
+                to_date(concat_ws("-", "year", "month", "day")),
+                col("temp").alias("avg_temperature_f"),
+                col("temp_count").alias("num_obs_temperature"),
+                col("max").alias("max_temperature_f"),
+                col("min").alias("min_temperature_f"),
+                col("dewp").alias("avg_dew_point_f"),
+                col("dewp_count").alias("num_obs_dew_point"),
+                col("slp").alias("avg_sea_level_pressure_mb"),
+                col("slp_count").alias("num_obs_sea_level_pressure"),
+                col("visib").alias("avg_visibility_miles"),
+                col("visib_count").alias("num_obs_visibility"),
+                col("wdsp").alias("avg_wind_speed_kt"),
+                col("wdsp_count").alias("num_obs_wind_speed"),
+                col("mxspd").alias("max_wind_speed_kt"),
+                col("gust").alias("max_wind_gust_kt")
             )
 
+        df.write.mode("overwrite").format("delta") \
+            .option("optimizeWrite", "True") \
+            .saveAsTable(self.STATIONS_SILVER)
