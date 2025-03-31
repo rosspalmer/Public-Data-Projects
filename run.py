8cf4e13a9f34966eda1b9794874297236662f3e1
Ross Palmer <rossbpalmer@gmail.com>
2025-03-31 14:00:16 -0600

    add stations to scripts

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from pdp.noaa.gsom import GlobalSummaryOfMonthParse
 from pdp.noaa.stations import SurfaceWeatherStations
 
-# job = SurfaceWeatherStations("/mnt/lake-fs/raw/gov/noaa/ncei")
+print("stations")
+job = SurfaceWeatherStations("/mnt/lake-fs/raw/gov/noaa/ncei")
+job.run()
+
+print("gsom-parse")
 job = GlobalSummaryOfMonthParse("/mnt/lake-fs/raw/gov/noaa/ncei/gsom")
 job.run()
