3d38cd2319b27955d2c81df3a367f1cbfeec8af8
Ross Palmer <rossbpalmer@gmail.com>
2025-03-31 14:41:31 -0600

    add explode to stations job

--- pdp/noaa/stations.py
+++ pdp/noaa/stations.py
@@ -48,7 +48,7 @@ class SurfaceWeatherStations(SparkJob):
             .select(
                 F.col("ghcn_id"), F.col("wmo_id"), F.col("name"),
                 F.col("lat"), F.col("long"), F.col("elevation"),
-                F.col("lookup")
+                F.explode(F.col("lookup"))
             )
         )
 
