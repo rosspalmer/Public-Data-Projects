a7d329962bc6d6c44c6c7214b4d77eb5a66a3c0b
Ross Palmer <rossbpalmer@gmail.com>
2025-03-31 14:06:59 -0600

    setup coordinates

--- pdp/noaa/stations.py
+++ pdp/noaa/stations.py
@@ -34,7 +34,8 @@ class SurfaceWeatherStations(SparkJob):
     def transform(self, data: DataSet) -> DataSet:
 
         def lookup_map(lat: float, long: float) -> dict:
-            return reverse_geocode.search((lat, long))
+            coordinates = lat, long
+            return reverse_geocode.search(coordinates)
         lookup_udf = F.udf(lookup_map)
 
         raw = data.get_table("raw_global_stations").df
