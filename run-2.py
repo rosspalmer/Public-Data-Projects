4cfebc598fdf6380c70176c754abc4239e11fe58
Ross Palmer <rossbpalmer@gmail.com>
2025-04-09 15:41:55 -0600

    only run last two jobs

--- run-2.py
+++ run-2.py
@@ -5,9 +5,9 @@ from pdp.data.noaa.gsom import TrendStationsQualified, GlobalMonthlyWeatherTrend
 # from pdp.data.noaa.stations import SurfaceWeatherStations
 
 with SparkJob("big-homes") as job:
-    parse = ParseGlobalSummaryOfMonth("/mnt/lake-fs/raw/gov/noaa/ncei/gsom")
+    # parse = ParseGlobalSummaryOfMonth("/mnt/lake-fs/raw/gov/noaa/ncei/gsom")
     # stations = SurfaceWeatherStations("")
-    monthly_weather = GlobalMonthlyWeather()
-    monthly_weather_trends = GlobalMonthlyWeatherTrends()
+    # monthly_weather = GlobalMonthlyWeather()
+    # monthly_weather_trends = GlobalMonthlyWeatherTrends()
     stations_qualified = TrendStationsQualified()
     frontend = GlobalMonthlyWeatherTrendsFrontend()
