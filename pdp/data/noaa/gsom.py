8b503a237820de47c230015a3af7edaeca14e55d
Ross Palmer <rossbpalmer@gmail.com>
2025-04-10 14:15:24 -0600

    remove year casting

--- pdp/data/noaa/gsom.py
+++ pdp/data/noaa/gsom.py
@@ -260,8 +260,7 @@ class GlobalMonthlyWeatherTrendsFrontend(SparkTask):
 
         stations = read_data.get_table("global_stations_trend_counts").df
         measurements = read_data.get_table("global_monthly_weather").df
-        trends = read_data.get_table("global_monthly_weather_rolling").df \
-            .withColumn("year", F.col("year").cast("int")) # FIXME year should be int in rolling table
+        trends = read_data.get_table("global_monthly_weather_rolling").df
 
         stations = stations.filter("temperature_count_avg10 >= 1000").select("ghcn_id")
 
