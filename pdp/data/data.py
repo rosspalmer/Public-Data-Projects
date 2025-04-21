0265b1dcef0381a4294e311553c43a5fa8b990e5
Ross Palmer <rossbpalmer@gmail.com>
2025-04-21 13:17:18 -0600

    add driver to jdbc call

--- pdp/data/data.py
+++ pdp/data/data.py
@@ -60,7 +60,7 @@ class DataSet:
         .jdbc(
             url="jdbc:mysql://10.0.0.85:3306",
             table=f"{table.schema}.{table.name}",
-            properties={"user": "ross", "password": "p@sswor!"}
+            properties={"user": "ross", "password": "p@sswor!", "driver": "org.mariadb.jdbc.Driver"}
         )
 
     def write_all_jdbc(self):
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from pdp.data.noaa.stations import SurfaceWeatherStations, StationClusters, Stat
 with SparkJob("big-homes") as job:
     # parse = ParseGlobalSummaryOfMonth("/mnt/lake-fs/raw/gov/noaa/ncei/gsom")
     # stations = SurfaceWeatherStations("/mnt/lake-fs/raw/gov/noaa/ncei")
-    station_clusters = StationClusters()
+    # station_clusters = StationClusters()
     station_jdbc = StationClustersFrontend()
     # monthly_weather = GlobalMonthlyWeather()
     monthly_weather_cluster = GlobalMonthlyWeatherClusters()
