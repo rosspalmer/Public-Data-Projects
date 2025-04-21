931da1e0d31c0234ce3f356e3c7b8c95d24aaf93
Ross Palmer <rossbpalmer@gmail.com>
2025-04-21 12:14:11 -0600

    fix keys

--- pdp/data/noaa/stations.py
+++ pdp/data/noaa/stations.py
@@ -141,6 +141,7 @@ class StationClusters(SparkTask):
             centermost_point = min(coord_list, key=lambda point: great_circle(point, centroid).m)
             df = pd.DataFrame({
                 "cluster_id": keys[0],
+                "network_id": keys[1],
                 "center_lat": [centermost_point[0]],
                 "center_long": [centermost_point[1]]
             })
@@ -151,15 +152,17 @@ class StationClusters(SparkTask):
 
         cluster_centers = (
             cluster_assignments
-            .drop("network_id")
-            .withColumn("ghcn_id", F.explode("station_ids"))
+            .select("cluster_id", F.explode("station_ids").alias("ghcn_id"))
             .join(stations.select("ghcn_id", "lat", "long"), "ghcn_id")
-            .groupby("cluster_id")
+            .groupby("cluster_id", "network_id")
             .applyInPandas(
                 calculate_cluster_center,
-                "cluster_id string, center_lat float, center_long float"
+                "cluster_id string, network_id string, center_lat float, center_long float"
             )
         ).persist()
+
+        # FIXME remove debug
+        cluster_centers.show()
 
         station_clusters = (
             cluster_assignments
