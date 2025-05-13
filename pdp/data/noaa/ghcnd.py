9722b609b376c41bcc2955c859a6e21e59f448e1
Ross Palmer <rossbpalmer@gmail.com>
2025-05-13 14:14:38 -0600

    fix schema definition2

--- pdp/data/noaa/ghcnd.py
+++ pdp/data/noaa/ghcnd.py
@@ -123,7 +123,7 @@ class GHCNDTransformedValues(SparkTask):
 
         measurement_lookups = spark.createDataFrame(
             data=self.MEASUREMENT_COLUMNS,
-            schema="ELEMENT str, name str, multiplier decimal(3, 5)"
+            schema="ELEMENT string, name string, multiplier decimal(3, 5)"
         )
 
         value_cols = [c for c in raw.columns if c.startswith("VALUE")]
