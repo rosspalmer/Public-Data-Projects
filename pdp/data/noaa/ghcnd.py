bb5243deafe65cad7e3ad3ff3d29aa93e17b14ea
Ross Palmer <rossbpalmer@gmail.com>
2025-05-13 14:09:50 -0600

    add transform job

--- pdp/data/noaa/ghcnd.py
+++ pdp/data/noaa/ghcnd.py
@@ -66,3 +66,78 @@ class GHCNDParseTextFiles(SparkTask):
         return DataSet([DataTable(
             "weather", "raw_ghcnd", parsed, "overwrite"
         )])
+
+
+class GHCNDTransformedValues(SparkTask):
+
+    # Defines measurements (decimals) to make as wide form columns,
+    # includes full name (with unit) and multiplier to get whole units
+    # (some values given in tenths or 10X)
+    MEASUREMENT_COLUMNS = [
+
+        # Temperature measurements
+        ("TMAX", "temperature_max_c", 10),
+        ("TMIN", "temperature_min_c", 10),
+        ("TAVG", "temperature_avg_c", 10),
+        ("ADPT", "temperature_avg_dew_point_c", 10),
+        ("AWBT", "temperature_avg_wet_bulb_c", 10),
+
+        # Pressure measurements
+        ("ASLP", "pressure_sea_level_hpa", 0.1),
+        ("ASTP", "pressure_station_level_hpa", 0.1),
+
+        # Humidity measurements
+        ("RHAV", "relative_humidity_avg_pct", 1),
+        ("RHMN", "relative_humidity_min_pct", 1),
+        ("RHMX", "relative_humidity_max_pct", 1),
+
+        # Precipitation measurements
+        ("PRCP", "precipitation_mm", 10),
+        ("SNOW", "snowfall_mm", 1),
+        ("SNWD", "snow_depth_mm", 1),
+        ("EVAP", "evaporation_mm", 10),
+
+        # Cloud cover measurements
+        ("ACMC", "cloudy_all_ceilo_avg_pct", 1),
+        ("ACMH", "cloudy_all_manual_avg_pct", 1),
+        ("ACSC", "cloudy_day_ceilo_avg_pct", 1),
+        ("ACSH", "cloudy_day_manual_avg_pct", 1),
+        ("PSUN", "sunshine_daily_pct", 1),
+
+        # Wind measurements
+        ("AWDR", "wind_direction_avg_degrees", 1),
+        ("AWND", "wind_speed_avg_ms", 10),
+	   
+    ]
+
+    def __init__(self):
+        super().__init__("ghcnd-transformed")
+
+    def read(self, spark: SparkSession) -> DataSet:
+        raw = DataTable("weather", "raw_ghcnd")
+        return DataSet([raw])
+
+    def transform(self, spark: SparkSession, read_data: DataSet) -> DataSet:
+
+        raw = read_data.get_table('raw_ghcnd').df
+
+        measurement_lookups = spark.createDataFrame(
+            data=self.MEASUREMENT_COLUMNS,
+            schema="ELEMENT str, name str, multiplier decimal<2, 3>"
+        )
+
+        value_cols = [c for c in raw.columns if c.startswith("VALUE")]
+
+        long_form_values = (
+            raw.melt(id_vars=["ID", "YEAR", "MONTH", "ELEMENT"],
+                     values=value_cols,
+                     variableColumnName="DAY")
+            .withColumn("DAY", F.regexp_extract("DAY", "VALUE(\\d+)").cast("int"))
+            .withColumn("date", F.make_date("YEAR", "MONTH", "DAY"))
+            .join(measurement_lookups, "ELEMENT", "inner")
+            .withColumn("value", F.col("value") * F.col("multiplier"))
+        )
+
+        values_table = DataTable("weather", "global_daily", long_form_values, "overwrite")
+
+        return DataSet([values_table])
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from pdp.data.job import SparkJob
-from pdp.data.noaa.ghcnd import GHCNDParseTextFiles
+from pdp.data.noaa.ghcnd import GHCNDParseTextFiles, GHCNDTransformedValues
 from pdp.data.noaa.gsom import (GSOMByStation, GSOMStationGroups,
                                 GlobalMonthlyWeatherTrends, GlobalMonthlyWeatherTrendsFrontend)
 from pdp.data.noaa.stations import SurfaceWeatherStations, StationGroups, StationGroupsFrontend
@@ -14,4 +14,5 @@ with SparkJob("big-homes") as job:
     # gsom_by_station_group = GSOMStationGroups()
     # monthly_weather_trends = GlobalMonthlyWeatherTrends("monthly_by_group")
     # frontend = GlobalMonthlyWeatherTrendsFrontend()
-    daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
+    # daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
+    daily_transformed = GHCNDTransformedValues()
