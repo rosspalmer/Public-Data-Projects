169989a7b66dc7a8e09c4f3dc3bc8741a58a4691
Ross Palmer <rossbpalmer@gmail.com>
2025-05-20 21:49:43 -0600

    fix path

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from pdp.data.weather.stations import SurfaceWeatherStation
 
 with SparkJob("big-homes") as job:
     # parse = ParseGlobalSummaryOfMonth("/mnt/lake-fs/raw/gov/weather/ncei/gsom")
-    stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/weather/ncei/ghcnd")
+    stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
     # station_clusters = StationGroups()
     # station_jdbc = StationGroupsFrontend()
     # gsom_by_station = GSOMByStation()
