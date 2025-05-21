bd3dc7d424b2c4d0da407753bf89c259d17e47ee
Ross Palmer <rossbpalmer@gmail.com>
2025-05-20 21:16:08 -0600

    read stations / history data using parser function

--- pdp/data/weather/stations.py
+++ pdp/data/weather/stations.py
@@ -14,9 +14,11 @@ import reverse_geocode
 
 from pdp.data.data import DataSet, DataTable
 from pdp.data.job import SparkTask
+from pdp.utils import parse_fixed_width
 
 
-class SurfaceWeatherStations(SparkTask):
+class SurfaceWeatherStation(SparkTask):
+
     NETWORK_ID_NAMES = {
         "0": "unspecified",
         "1": "community_rain_hail_snow",
@@ -29,6 +31,26 @@ class SurfaceWeatherStations(SparkTask):
         "W": "wban"
     }
 
+    STATION_COLUMNS = [
+        ("ghcn_id", 11, "string"),
+        ("latitude", 9, "float"),
+        ("longitude", 10, "float"),
+        ("elevation", 7, "float"),
+        ("state", 3, "string"),
+        ("gsn_flag", 4, "string"),
+        ("hcn_crn_flag", 4, "string"),
+        ("wmo_id", 6, "string")
+    ]
+
+    STATION_HISTORY_COLUMNS = [
+        ("ghcn_id", 11, "string"),
+        ("latitude", 9, "float"),
+        ("longitude", 10, "float"),
+        ("element", 5, "string"),
+        ("first_year", 5, "int"),
+        ("last_year", 5, "int")
+    ]
+
     def __init__(self, ncei_data_folder: str):
         super().__init__("surface-stations")
         self.ncei_data_folder = ncei_data_folder
@@ -37,46 +59,75 @@ class SurfaceWeatherStations(SparkTask):
 
         spark.sql("CREATE SCHEMA IF NOT EXISTS weather")
 
-        raw = (
+        raw_station = (
             spark
             .read
-            .csv(f"{self.ncei_data_folder}/ghcnd-stations.csv")
-            .withColumnsRenamed({
-                "_c0": "ghcn_id", "_c1": "lat", "_c2": "long",
-                "_c3": "elevation", "_c4": "state", "_c5": "name",
-                "_c6": "gsn", "_c7": "hcn_crn", "_c8": "wmo_id"
-            })
+            .text(f"{self.ncei_data_folder}/ghcnd-stations.txt")
+        )
+
+        raw_history = (
+            spark
+            .read
+            .text(f"{self.ncei_data_folder}/ghcnd-inventory.txt")
         )
 
         return DataSet([
-            DataTable("weather", "raw_global_stations", raw, "overwrite")
+            DataTable("weather", "raw_station", raw_station, "overwrite"),
+            DataTable("weather", "raw_station_history", raw_history, "overwrite")
         ])
 
     def transform(self, spark: SparkSession, read_data: DataSet) -> DataSet:
 
+        raw_station_parsed = parse_fixed_width(
+            read_data.get_table("raw_station").df,
+            self.STATION_COLUMNS
+        )
+
+        print("STATION PARSED")
+        raw_station_parsed.show()
+
+        raw_station_history_parsed = parse_fixed_width(
+            read_data.get_table("raw_station_history").df,
+            self.STATION_HISTORY_COLUMNS
+        )
+
+        print("STATION HISTORY PARSED")
+        raw_station_history_parsed.show()
+
         network_name_map = {k: v for k, v in self.NETWORK_ID_NAMES.items()}
         network_name_udf = F.udf(lambda x: network_name_map.get(x), StringType())
 
-        raw = (
-            read_data.get_table("raw_global_stations").df
-            .withColumn("name", F.trim("name"))
+        # TODO Parse out columns for station data
+        parsed_and_enriched = (
+            raw_station_parsed
+            .withColumns({
+                name: F.trim(name)
+                for name, width, data_type in self.STATION_COLUMNS
+                if data_type == "string"
+            })
             .withColumn("country_code", F.left("ghcn_id", F.lit(2)))
             .withColumn("network_id", F.substring("ghcn_id", 3, 1))
             .withColumn("network_name", network_name_udf(F.col("network_id")))
             .withColumn("wban_id", F.when(F.col("network_id") == "W", F.right("ghcn_id", F.lit(5))))
         )
 
-        stations = [(r.ghcn_id, (r.lat, r.long))
-                    for r in raw.select("ghcn_id", "lat", "long").collect()]
-        ids = [x[0] for x in stations]
-        coords = [x[1] for x in stations]
+        # Build station and coordinate lists for reverse_geocode lookup below
+        station_coords_data = [
+            (r.ghcn_id, (r.lat, r.long))
+            for r in raw_station_parsed.select("ghcn_id", "latitude", "longitude").collect()
+        ]
+        station_ids = [x[0] for x in station_coords_data]
+        coords = [x[1] for x in station_coords_data]
 
-        lookups = zip(ids, reverse_geocode.search(coords))
+        # Create DataFrame using reverse_geocode 'search'
+        # on station coordinates (`data` map<string, string> column)
+        lookups = zip(station_ids, reverse_geocode.search(coords))
         lookup_df = spark.createDataFrame(
             data=lookups,
             schema="ghcn_id string, data map<string, string>"
         ).persist()
 
+        # Create list of unique key names from `data` map for use below
         data_keys = [r.data_key for r in (
             lookup_df.select(
                 F.explode(
@@ -89,25 +140,21 @@ class SurfaceWeatherStations(SparkTask):
         for k in data_keys:
             lookup_df = lookup_df.withColumn(k, F.element_at("data", k))
 
-        lookup_df = lookup_df.drop("data")
-
-        cast_type = {
-            "lat": "float", "long": "float",
-            "latitude": "float", "longitude": "float",
-            "elevation": "long", "population": "long"
-        }
-
-        with_geo_data = (
-            raw
-            .drop("state")
-            .join(lookup_df, "ghcn_id", "left")
-            .withColumns({k: F.col(k).cast(v) for k, v in cast_type.items()})
+        lookup_df = (
+            lookup_df
+            .drop("data")
             .withColumnsRenamed({"latitude": "city_lat", "longitude": "city_long"})
         )
 
+        station_with_geo_data = (
+            parsed_and_enriched
+            .drop("state")
+            .join(lookup_df, "ghcn_id", "left")
+        )
+
         return DataSet([
-            DataTable("weather", "global_stations", with_geo_data, "overwrite"),
-            read_data.get_table("raw_global_stations")
+            DataTable("weather", "station", station_with_geo_data, "overwrite"),
+            DataTable("weather", "station_history", raw_station_history_parsed, "overwrite")
         ])
 
 
