074fe5a4a4f35dccfdc63ec89ed1e376d1fd03e4
Ross Palmer <rossbpalmer@gmail.com>
2025-05-21 22:19:21 -0600

    run areas job

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from pdp.data.job import SparkJob
-from pdp.data.weather.ghcnd import GHCNDParseTextFiles, GHCNDailyByStation
-from pdp.data.weather.gsom import (GSOMByStation, GSOMStationGroups,
-                                   GlobalMonthlyWeatherTrends, GlobalMonthlyWeatherTrendsFrontend)
+from pdp.data.weather.areas import WeatherAreas
+from pdp.data.weather.ghcnd import GHCNDailyByStation
 from pdp.data.weather.seasons import GlobalWeatherSeasonByStation
 from pdp.data.weather.stations import SurfaceWeatherStation
 
@@ -9,12 +8,7 @@ from pdp.data.weather.stations import SurfaceWeatherStation
 with SparkJob("big-homes") as job:
     # parse = ParseGlobalSummaryOfMonth("/mnt/lake-fs/raw/gov/weather/ncei/gsom")
     stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
-    # station_clusters = StationGroups()
-    # station_jdbc = StationGroupsFrontend()
-    # gsom_by_station = GSOMByStation()
-    # gsom_by_station_group = GSOMStationGroups()
-    # monthly_weather_trends = GlobalMonthlyWeatherTrends("monthly_by_group")
-    # frontend = GlobalMonthlyWeatherTrendsFrontend()
+    areas = WeatherAreas()
     # daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
     # daily_transformed = GHCNDailyByStation()
     # seasons = GlobalWeatherSeasonByStation()
