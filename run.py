6550eaa7582d97939a6add05051aaaa1887c2d4b
Ross Palmer <rossbpalmer@gmail.com>
2025-05-21 22:21:30 -0600

    run cleanup

--- run.py
+++ run.py
@@ -6,9 +6,10 @@ from pdp.data.weather.stations import SurfaceWeatherStation
 
 
 with SparkJob("big-homes") as job:
-    # parse = ParseGlobalSummaryOfMonth("/mnt/lake-fs/raw/gov/weather/ncei/gsom")
+
     stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
     areas = WeatherAreas()
+
     # daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
     # daily_transformed = GHCNDailyByStation()
     # seasons = GlobalWeatherSeasonByStation()
