cfbe34d827d0863c0dc0b32acc6bf3c5c4e69995
Ross Palmer <rossbpalmer@gmail.com>
2025-05-27 15:27:31 -0600

    move run.py into new jobs module

--- pdp/jobs/weather.py
+++ pdp/jobs/weather.py
@@ -5,13 +5,13 @@ from pdp.data.weather.seasons import WeatherSeasonByArea, WeatherSeasonByStation
 from pdp.data.weather.stations import SurfaceWeatherStation
 
 
-with SparkJob("big-homes") as job:
+with SparkJob("weather") as job:
 
     stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
     areas = WeatherAreas()
 
     # daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
-    # daily_transformed = GHCNDailyByStation()
+    daily_transformed = GHCNDailyByStation()
     season_station = WeatherSeasonByStation()
     season_area = WeatherSeasonByArea()
     season_trends = WeatherSeasonTrends("area")
