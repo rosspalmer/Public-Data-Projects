21410874cd750dd4ae977ca1b4ec811d8dd8c473
Ross Palmer <rossbpalmer@gmail.com>
2025-05-27 18:19:51 -0600

    check in full job

--- pdp/jobs/weather.py
+++ pdp/jobs/weather.py
@@ -1,18 +1,18 @@
 from pdp.data.job import SparkJob
 from pdp.data.weather.areas import WeatherAreas
-from pdp.data.weather.ghcnd import GHCNDailyByStation
+from pdp.data.weather.ghcnd import GHCNDailyByStation, GHCNDParseTextFiles
 from pdp.data.weather.seasons import WeatherSeasonByArea, WeatherSeasonByStation, WeatherSeasonTrends
 from pdp.data.weather.stations import SurfaceWeatherStation
 
 
 with SparkJob("weather") as job:
 
-    # stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
-    # areas = WeatherAreas()
+    stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
+    areas = WeatherAreas()
 
-    # daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
-    # daily_transformed = GHCNDailyByStation()
-    # season_station = WeatherSeasonByStation()
-    # season_area = WeatherSeasonByArea()
+    daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
+    daily_transformed = GHCNDailyByStation()
+    season_station = WeatherSeasonByStation()
+    season_area = WeatherSeasonByArea()
     season_station_trends = WeatherSeasonTrends("station")
     season_area_trends = WeatherSeasonTrends("area")
