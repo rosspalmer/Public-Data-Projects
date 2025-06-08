9cbea547a9e4578722a087c5936af6f66d412efe
Ross Palmer <rossbpalmer@gmail.com>
2025-06-08 13:05:58 -0600

    run full job

--- pdp/jobs/weather.py
+++ pdp/jobs/weather.py
@@ -7,12 +7,12 @@ from pdp.data.weather.stations import SurfaceWeatherStation
 
 with SparkJob("weather") as job:
 
-    # stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
-    # areas = WeatherAreas()
+    stations = SurfaceWeatherStation("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd")
+    areas = WeatherAreas()
 
-    # daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
-    # daily_transformed = GHCNDailyByStation()
-    # season_station = WeatherSeasonByStation()
-    # season_area = WeatherSeasonByArea()
+    daily_parse = GHCNDParseTextFiles("/mnt/lake-fs/raw/gov/noaa/ncei/ghcnd/ghcnd_all")
+    daily_transformed = GHCNDailyByStation()
+    season_station = WeatherSeasonByStation()
+    season_area = WeatherSeasonByArea()
     season_station_trends = WeatherSeasonTrends("station")
     season_area_trends = WeatherSeasonTrends("area")
